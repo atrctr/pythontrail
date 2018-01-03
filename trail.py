54f0c44cc6893d16cc7ba215bec179c63c87b6ef
Kacper Kowalczyk <kacper.k.kowalczyk@gmail.com>
2018-01-03 20:19:52 +0000

    Added more displays and revamped victory conditions to match the original game

--- trail.py
+++ trail.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-# Welcome to Python Trail I guess
-
 import random
 import string
 
@@ -9,36 +7,46 @@ import string
 
 Day = 1
 People = 10
-SupplyDays = 14
+SupplyDays = 14 # 1 gets used per day
 Money = 100
+Distance = 0
+Destination = 1000 # miles 
+BaseSpeed = 20 # miles per day
 
 # Intro sequence
 
-print("\n\tPython Trail of Tears")
+print("\n\tPython Trail of Tears\n\nYou win by reaching the Destination,\n1000 miles away from home. Good luck!")
 
 # Main game loop
-while Day < 365:
-    # Normal ordinary turn when everyone is alive
+while Distance < Destination:
+
+    # Show all status information
+    print("\n### DAY",Day,"###########################")
+    print("Travelled",Distance,"miles")
+    print("Survivors:",People,"\tSupplies:",SupplyDays,"\tMoney:",Money,"\n")
+
+    # If everyone is dead, game over
+    if People <= 0:
+        print("GAME OVER: All surivors are dead.")
+        break
+
+    # Normal ordinary turn when proceed with the loop
     if People > 0:
-        print("\n### DAY",Day,"###")
-        print("Survivors:",People,"\tSupplies:",SupplyDays,"\tMoney:",Money)
+
         Day = Day + 1
+        Speed = BaseSpeed
+        Distance = Distance + Speed
         if SupplyDays > 0:
             SupplyDays = SupplyDays - 1
         elif SupplyDays <= 0:
             People = People - 1
-    # Everyone is dead.
-    if People <= 0:
-        print("GAME OVER\nEveryone has died.")
-        break
+
 
 # If on day 365 some is still alive...
 if People > 0:
     print("Day",Day)
     print("VICTORY")
 
-
 # Game end
-print("\n\tEnd of game")
 input("\n\tPress ENTER to exit")
 quit()
